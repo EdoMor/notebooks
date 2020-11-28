96cd1f1f667895196bd7b003c9ea0e3f5e4511a7
EdoMor <edo.mor@mail.huji.ac.il>
2020-11-28 14:35:36 +0200

    fixed naming scheme

--- ease_of_live_scripts.py
+++ ease_of_live_scripts.py
@@ -31,3 +31,15 @@ def Rsquared(x: list, data: list, funciton: Callable , popt: np.ndarray):
     residual_sum_of_squares = np.sum((np.array(data) - funciton(np.array(x), *popt)) ** 2)
     total_sum_of_squares = np.sum((np.array(data) - np.mean(data)) ** 2)
     return 1 - (residual_sum_of_squares / total_sum_of_squares)
+
+def csc(x):
+    return 1/np.sin(x)
+
+def single_slit_model(x,A,z,d,k):
+    return A*(2*z*np.sin((d*k*x)/(2*z))**2)/(k*np.pi*x**2)
+
+def double_slit_model(x,A,z,d,k,L):
+    return A*(8*z*np.cos((k*L*x)/(2*z))**2*np.sin((d*k*x)/(2*z))**2)/(k*np.pi*x**2)
+
+def n_slits_model(x,A,z,d,k,L,n):
+    return A*(2*z*csc((k*L*x)/(2*z))**2*np.sin((d*k*x)/(2*z))**2*np.sin((k*L*(1 + 2*n)*x)/(2*z))**2)/(k*np.pi*x**2)
